4669ae50410a8cf20c06f69593c8499c40cbcca5
jienba <adamjienba@outlook.fr>
2021-06-23 23:30:28 +0000

    Welcome OOP

---
+++ Day-16-Start/main.py
@@ -0,0 +1,17 @@
+# from turtle import Turtle, Screen
+#
+# timmy = Turtle()
+# timmy.shape("turtle")
+# timmy.color("blue1")
+#
+#
+# timmy.forward(100)
+# my_screen = Screen()
+# print(my_screen.canvheight)
+# my_screen.exitonclick()
+
+from prettytable import PrettyTable
+
+table = PrettyTable()
+
+print(table)
