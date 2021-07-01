a0dd62b450331a96c6d478909936a964b1f782a6
jienba <adamjienba@outlook.fr>
2021-07-01 15:04:28 +0000

    init commit day 18

---
+++ Day-18-Start/main.py
@@ -0,0 +1,26 @@
+from turtle import Turtle, Screen
+
+jack_the_turtle = Turtle()
+jack_the_turtle.shape('turtle')
+jack_the_turtle.color('red')
+for _ in range(4):
+    jack_the_turtle.forward(200)
+    jack_the_turtle.left(90)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+screen = Screen()
+screen.exitonclick()
